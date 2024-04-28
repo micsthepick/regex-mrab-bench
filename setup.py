2e3272be48e32367cb1110dba5329ace06312017
Matthew Barnett <git@mrabarnett.plus.com>
2024-04-28 20:11:20 +0100

    Git issue 527: `VERBOSE`/`X` flag breaks `\N` escapes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst', encoding='utf-8') as file:
 
 setup(
     name='regex',
-    version='2024.4.16',
+    version='2024.4.28',
     description='Alternative regular expression module, to replace re.',
     long_description=long_description,
     long_description_content_type='text/x-rst',
@@ -21,7 +21,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
@@ -32,7 +31,7 @@ setup(
         'Topic :: Text Processing',
         'Topic :: Text Processing :: General',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
 
     package_dir={'regex': 'regex_3'},
     py_modules=['regex.__init__', 'regex.regex', 'regex._regex_core',
